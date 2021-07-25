a771538679e10f19c55aea3e09dfd7ed07547fad
Yusues <83811592+Yusues@users.noreply.github.com>
2021-07-25 19:11:34 +0300

    Add files via upload

---
+++ movie/anasayfa.php
@@ -0,0 +1,36 @@
+<?php
+include "php/ust.php";
+?>  
+     
+    <body style = "background: url(resimler/Background.jpg)"></body>
+  </div>
+  <div id="biggie">
+    <aside id="imdb">
+      <div id="imbd-ara"> <p style="border:groove">İmdb Ara</p> </div>
+      <div id="imbd-top"> İMDB POPÜLER</div>
+    </aside>
+    <div id="icerik">
+            <nav id="menu-button" >
+        <ul>
+		  <a href="postpage.html" target="ana"> <li>Anasayfa</li></a>
+			<li>Kategoriler
+			<ul>
+				<a href="imdb/form.html" target="ana"><li>Filmler</li></a>
+				<li>Diziler</li>
+			</ul>
+			</li>
+			<a href="top10/topten.html" target="ana"><li>TOP 10</li></a>
+			<a href="php/chat.php" target="ana"><li>Eleştiriler</li></a>
+		  
+		  
+		  </ul>
+      </nav>
+		<iframe src="postpage.html" name="ana" width="100%" height="96%" frameborder="0"></iframe>
+		
+      </div>
+    <div id="alt-sag"> <nav id="site-ara"><p style="border:groove">Site içi ara</p> </nav>haftanın popülerleri 
+	  
+	  </div>
+ <?php
+include "php/alt.php";
+?>  
